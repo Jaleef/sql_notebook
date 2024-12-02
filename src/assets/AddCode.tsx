81a3a232abf51aebe873b79b905dd772bd7fb236
YanJiaLe <jeffyan167@gmail.com>
2024-12-02 13:14:02 +0800

    添加了 + code按键和 + markdown按键

---
+++ src/assets/AddCode.tsx
@@ -0,0 +1,5 @@
+function AddCode() {
+	return <div className="border-2 border-gray-300 px-2">+ Code</div>;
+}
+
+export default AddCode;
---
+++ src/assets/AddMarkdown.tsx
@@ -0,0 +1,5 @@
+function AddMarkdown() {
+	return <div className="border-2 border-gray-300 px-2">+ Markdown</div>;
+}
+
+export default AddMarkdown;
