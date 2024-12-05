aeafa77a1eb10dc14cad8f26fe8ac601b2013550
YanJiaLe <jeffyan167@gmail.com>
2024-12-05 13:10:43 +0800

    根组件App添加了boxList状态列表, 来渲染多个box组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import CodeBox from "./components/CodeBox";
+import { useState } from "react";
+
+let id = 0;
 function App() {
+  const initialBoxList = [
+    <CodeBox key={id} boxId={id.toString()} />,
+  ];
+  id++;
+
+  const [boxList, setBoxList] = useState(initialBoxList);
+
+
   return (
     <div className="container mx-auto px-52 py-5 bg-white">
-      <CodeBox />
+      <div className="flex flex-col">
+        {
+          boxList
+        }
+      </div>
     </div>
   );
 }
