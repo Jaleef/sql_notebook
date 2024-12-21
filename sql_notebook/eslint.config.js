08eab224c86575e6a1adcf1a19f12cbfe1b8dca5
YanJiaLe <jeffyan167@gmail.com>
2024-12-21 19:06:27 +0800

    修改了eslint的配置文件

--- sql_notebook/eslint.config.js
+++ sql_notebook/eslint.config.js
@@ -12,6 +12,9 @@ export default tseslint.config(
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.app.json', './tsconfig.node.json']
+      }
     },
     plugins: {
       'react-hooks': reactHooks,
