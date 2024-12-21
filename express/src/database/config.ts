fc8809b3cb1e7749eb3771246a306f04f4b8379a
YanJiaLe <jeffyan167@gmail.com>
2024-12-21 13:41:53 +0800

    用类型重写了连接mysql2数据库的配置对象

--- express/src/database/config.ts
+++ express/src/database/config.ts
@@ -1,8 +1,8 @@
-export const config = {
-  db: {
-    host: '127.0.0.1',
-    port: '3306',
-    user: 'root',
-    password: '17321852066Yjl@',
-  }
-}
+import { ConnectionOptions } from "mysql2";
+
+export const db: ConnectionOptions= {
+	host: "127.0.0.1",
+	port: 3306,
+	user: "root",
+	password: "17321852066Yjl@",
+};
